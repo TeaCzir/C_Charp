bee9766fd6a4325272ff74ec40e1f17e18b37727
TeaCzir <teaczir@gmail.com>
2023-07-15 22:40:47 +0300

    Дни недели.

---
+++ Task02_Week_day/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая будет выдавать название дня недли
+по заданному числу.*/
+
+Console.Clear();
+Console.Write("Введите число дня недели: ");
+int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {DayOfWeek};");
+
+while (DayOfWeek < 1 || DayOfWeek > 7)
+{
+   Console.WriteLine("Введено неправильное число.");
+   Console.Write("Введите правильное число дня недели(от 1 до 7). ");
+   break;
+}
+
+if (DayOfWeek == 1) Console.WriteLine("Это будет: Понедельник.");
+if (DayOfWeek == 2) Console.WriteLine("Это будет: Вторник.");
+if (DayOfWeek == 3) Console.WriteLine("Это будет: Среда.");
+if (DayOfWeek == 4) Console.WriteLine("Это будет: Четверг.");
+if (DayOfWeek == 5) Console.WriteLine("Это будет: Пятница.");
+if (DayOfWeek == 6) Console.WriteLine("Это будет: Суббота.");
+if (DayOfWeek == 7) Console.WriteLine("Это будет: Воскресенье.");
