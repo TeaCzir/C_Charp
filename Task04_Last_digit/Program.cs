33f1f570c2d0e9054dcca8cd3ce1a305a293a9f2
TeaCzir <teaczir@gmail.com>
2023-07-15 23:52:21 +0300

    Последняя цифра от трехзначного числа.

---
+++ Task04_Last_digit/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите программу, которая на вход принимает трехзначное 
+число, а на выход показывает последнюю цифру этого числа.*/
+Console.Clear();
+Console.Write("Введите трехзначое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {num};");
+
+int LastDigit = num % 10;
+Console.Write($"Последняя цифра: {LastDigit};");
