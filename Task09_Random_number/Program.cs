0bd0cbe48f9a524a449c72b2aa3c54830d8b2205
TeaCzir <teaczir@gmail.com>
2023-07-16 01:15:51 +0300

    Наибольшая цифра из двухзначного рандомного числа.

---
+++ Task09_Random_number/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая выводит случайное число из
+отрезка [10, 99] и показывает наибольшую цифру этого числа.
+*/
+Console.Clear();
+int randnum = new Random().Next(10, 100);
+
+Console.WriteLine($"Случайное число: {randnum};");
+int past = randnum % 10;
+int first = randnum / 10;
+
+int max = past;
+
+if (first > past) max = first;
+
+Console.Write($"Наибольшая цифра числа: {max};");
