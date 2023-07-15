4b91ee8bfbba2c013d5b1050ec64cf70c95d0bd9
TeaCzir <teaczir@gmail.com>
2023-07-16 01:39:48 +0300

    Вторая цифра от трехзначного числа.

---
+++ Task10_Second_digit/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая принимает на вход трехзначное 
+число и на выходе показывает вторую цифру этого числа.*/
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {num};");
+int lastwo = num % 100;
+int second = lastwo /10;
+
+Console.Write($"Вторая цифра числа: {second};");
+
