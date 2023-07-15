69f470609d9b9c783e4e9884ee3308b8062592f2
TeaCzir <teaczir@gmail.com>
2023-07-15 17:43:21 +0300

    Квадрат - не квадрат одно число другому.

---
+++ Task00_Square_or_not/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая на вход принимает два числа и 
+проверяет, является ли первое квадратом второго.*/
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.Write($"Вы ввели число: {num1} и число: {num2};");
+Console.WriteLine();
+
+if (num1 == num2*num2)
+{
+   Console.Write($"Число: {num1} является квадратом числа: {num2};");
+}
+else 
+Console.Write($"Число: {num1} не квадрат числа: {num2};");
