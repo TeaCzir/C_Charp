e091bdaafe93dc5a390c9c94f6433d28176222f3
TeaCzir <teaczir@gmail.com>
2023-07-15 15:28:57 +0300

    Инициализация. Максимальное из двух и из трех.

---
+++ Task01_Max/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая на вход принимает два числа
+и выдает, какое число больше, а какое меньше.*/
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.Write($"Вы ввели числа: {num1}, {num2};");
+Console.WriteLine();
+int max = num1;
+int min = num2;
+if(num2 > num1)
+{
+max = num2;
+Console.Write($"Максимальное число: {num2}, ");
+
+Console.Write($"а число: {num1} минимальное.");
+} 
+else
+{
+max = num1;
+Console.Write($"Максимальное число: {num1}, ");
+
+Console.Write($"а число: {num2} минимальное.");
+}
---
+++ Task01_Max_of_three/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая на вход принимает три числа 
+и выдает максимальное из этих трех чисел.*/
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.Write($"Вы ввели три числа: {num1}, {num2}, {num3};");
+Console.WriteLine();
+int max = num1;
+if(num2 > num1) max = num2;
+if(num3 > num2) max = num3;
+Console.Write($"Максимальное число: {max};");
