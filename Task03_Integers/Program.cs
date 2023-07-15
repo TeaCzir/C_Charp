1e6860d4854970407cf713ff24a3464d04dbf3e5
TeaCzir <teaczir@gmail.com>
2023-07-15 23:05:39 +0300

    Целые числа от -N до N.

---
+++ Task03_Integers/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая на вход принимает одно
+число, а на выходе показывает все целые числа от - N до N.*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {num};");
+Console.WriteLine($"Числа от -{num} до {num}:");
+for (int i = -num; i < num + 1; i++)
+{
+Console.Write($"{i}; ");
+}
