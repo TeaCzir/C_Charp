7d64bff126e1d6625b7e29feabf872096316637a
TeaCzir <teaczir@gmail.com>
2023-07-15 16:34:17 +0300

    Четные числа от 1 до N.

---
+++ Task03_Even_numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а
+на выходе показывает все четные числа от 1 до N.*/
+Console.Clear();
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {N};");
+Console.WriteLine($"Четные числа от 1 до {N}: ");
+Console.Write("(");
+for (int i = 2; i <= N; i+=2)
+{
+   Console.Write($"{i}");
+   Console.Write(", ");
+}
+Console.Write(");");
