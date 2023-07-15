590551e620fcab8063883ed91e62a3cb15d382d5
TeaCzir <teaczir@gmail.com>
2023-07-15 17:25:22 +0300

    Квадрат числа.

---
+++ Task00_Square/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая на вход принимает число и 
+выдает его квадрат.*/
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {num};");
+Console.WriteLine($"Квадрат числа: {num} равен {num * num};");
