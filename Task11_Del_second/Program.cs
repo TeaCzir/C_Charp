62b94ee677a3ac4c0892840a0b0d3d75eaf17527
TeaCzir <teaczir@gmail.com>
2023-07-16 16:50:58 +0300

    Удаляем вторую цифру из трехзачного числа.

---
+++ Task11_Del_second/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Напишите программу, которая выводит случайное трехзначное 
+число и удаляет вторую цифру этого числа.*/
+Console.Clear();
+int randnum = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число: {randnum};");
+int result = (randnum/100)*10 + randnum%10;
+Console.WriteLine($"Удаляем вторую цифру и получаем: {result};");
