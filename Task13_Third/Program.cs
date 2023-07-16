0d2b636ab26964b099551feb10112f7e1ca6b49c
TeaCzir <teaczir@gmail.com>
2023-07-16 20:42:51 +0300

    Выводим из заданного третью, а еслиее нет - выводим нет третьей.

---
+++ Task13_Third/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного 
+числа или сообщает, что третьей цифры нет.*/
+Console.Clear();
+Console.Write("Введите число в интервале от -2147483648 до 2147483647: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {num};");
+if (num / 100 == 0)
+   Console.Write($"В числе: {num} нет третьей цифры;");
+else
+{
+   while (num / 1000 != 0)
+   {
+      num = num / 10;
+   }
+   int Third = num % 10;
+   Console.Write($"Третья цифра: {Third};");
+}
+
