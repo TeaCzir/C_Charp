63c7e68c7c93d0c4c1b8f212cebb3a918b195472
TeaCzir <teaczir@gmail.com>
2023-07-16 21:07:22 +0300

    Кратно 7 и 23 одновременно.

---
+++ Task14_Multiple_of_two/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на вход число 
+и проверяет кратно ли оно одновременно 7 и 23.*/
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {num};");
+if (num % (7 * 23) == 0)
+
+   Console.Write($"Да, число: {num} кратно 7 и 23;");
+else
+   Console.Write($"Нет, число: {num} некратно одновременно 7 и 23;");
