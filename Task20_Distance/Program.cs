5f2d22801a24d04a7b5462a13c158b0702c6b29f
TeaCzir <teaczir@gmail.com>
2023-07-17 03:17:08 +0300

    Расстояние по координатам в 2D пространстве.

---
+++ Task20_Distance/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход координаты 
+двух точек и находит расстояние между ними в 2D пространстве.*/
+
+Console.Clear();
+Console.Write("Введите x1 ");
+double x1 = Convert.ToInt64(Console.ReadLine());
+Console.Write("Введите y1 ");
+double y1 = Convert.ToInt64(Console.ReadLine());
+Console.Write("Введите x2 ");
+double x2 = Convert.ToInt64(Console.ReadLine());
+Console.Write("Введите y2 ");
+double y2 = Convert.ToInt64(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели: точка A ({x1}, {y1}) и точка B ({x2}, {y2}),");
+// double d1 = Math.Sqrt(-4);
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+Console.WriteLine($"Расстояние от А до В равно {d:f2}.");
