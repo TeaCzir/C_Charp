a0ce9486788de6342a710b27ba91083bdc7f1541
TeaCzir <teaczir@gmail.com>
2023-07-18 16:14:37 +0300

    Таблица квадрадов до числа n.

---
+++ Task22_Table_of_squares/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и 
+выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {n},");
+Console.Write($"Таблица квадратов от 1 до {n} будет: ");
+int count = 1;
+while (count <=n)
+{
+Console.Write($"{Math.Pow(count, 2)}");
+   if (count !=n)
+   {
+      Console.Write($", ");
+   }
+   else 
+   {
+   Console.Write($";");
+   }
+   count++;
+}
