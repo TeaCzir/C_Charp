d8c7df0bbbedb9b8211fdda0a1d13febaf0b7b2a
TeaCzir <teaczir@gmail.com>
2023-07-20 02:50:15 +0300

    Сумма от 1 до А с использование метода и без.

---
+++ Task24_Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿/*/*Напишите программу, которая на вход принимает число А, 
+и на выход выдает сумму всех чисел от 1 го до А.*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {A};");
+
+int Sum = 0;
+for (int i = 1; i <= A; i++)
+{
+   Sum += i;
+}
+Console.Write($"Сумма чисел от 1 до А: {Sum};");
---
+++ Task24_Sum_method/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу с использованием метода, которая 
+на вход принимает число А, и на выход выдает сумму всех 
+чисел от 1 го до А.*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {A};");
+
+int Method(int s)
+{
+int sum = 0;
+for (int i = 1; i <= s; i++)
+{
+   sum+=i;//sum = sum + i;
+}
+return sum;
+}
+int Sumnum = Method(A);
+Console.Write($"Сумма чисел от 1 до А: {Sumnum};");
