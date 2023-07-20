cc5825c5162c2be795b8c2973bfc8ee55b5bf534
TeaCzir <teaczir@gmail.com>
2023-07-20 13:45:11 +0300

    Вывод массива и вывод его разворота.

---
+++ Task29_Re_arr/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Разворот массива в обратном порядке.*/
+Console.Clear();
+Console.Write("  Массив: ");
+
+void PrintArray(int[] array)
+{
+   Console.Write("[");
+   for (int i = 0; i < array.Length; i++)
+   {
+      Console.Write(array[i]);
+      if (i < array.Length - 1) Console.Write(", ");
+      if (i == array.Length - 1) Console.Write("");
+   }
+   Console.Write("]");
+   Console.WriteLine();
+   Console.Write("Разворот: ");
+   Console.Write("[");
+   for (int i = array.Length - 1; i >= 0; i--)
+   {
+      Console.Write(array[i]);
+      if (i >= 1) Console.Write(", ");
+      if (i == 0) Console.Write("");
+   }
+   Console.Write("]");
+}
+int[] array = new int[8];
+for (uint i = 0; i < array.Length; i++)
+{
+   array[i] = new Random().Next(10, 99);
+}
+PrintArray(array);
