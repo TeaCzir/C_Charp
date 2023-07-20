6fa3d03b14494afd2c80ae6f4eac8a73e5a8f163
TeaCzir <teaczir@gmail.com>
2023-07-21 00:08:09 +0300

    Замена отрицателных и полжительных перекрестно.

---
+++ Task32_Positive_to_negative/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Напишите программу замена элементов мвссива: положительные элементы 
+замените на соответствующие отрицательные, и напоборот.*/
+
+Console.Clear();
+Console.WriteLine("Массив до замены: ");
+
+int[] Array(int length, int deviation)
+{
+   int[] result = new int[length];
+   for (int i = 0; i < length; i++)
+   {
+      result[i] = new Random().Next(-deviation, deviation + 1);
+   }
+   return result;
+}
+
+void PrintArray(int[] farray)
+{
+   Console.Write("[");
+   for (int i = 0; i < farray.Length; i++)
+   {
+      Console.Write(farray[i]);
+      if (i != farray.Length - 1) Console.Write(", ");
+   }
+   Console.WriteLine("]");
+}
+int[] Rarray = Array(14, 21);
+PrintArray(Rarray);
+void Replacement(int[] num)
+{
+   for (int i = 0; i < num.Length; i++)
+   {
+      num[i] *= -1;
+   }
+}
+Console.WriteLine("Массив после замены: ");
+Replacement(Rarray);
+PrintArray(Rarray);
