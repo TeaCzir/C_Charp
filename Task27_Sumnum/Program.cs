df9f11ecac8343ad1f3504644d4aa845bc98edf4
TeaCzir <teaczir@gmail.com>
2023-07-20 04:59:29 +0300

    Сумма цифр в заданном числе.

---
+++ Task27_Sumnum/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт 
+сумму цифр в числе.*/
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {a};");
+int Num(int c)
+{
+   int sum = 0;
+   for (int i = 0; c > 0; i++)
+   {
+      sum += c % 10;
+      c /= 10;
+   }
+   return (sum);
+}
+Console.WriteLine($"Сумма цифр в числе равна: {Num(a)};");
