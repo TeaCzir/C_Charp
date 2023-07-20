a5602d0e0feb79498e2199fa2760d815b078d243
TeaCzir <teaczir@gmail.com>
2023-07-20 05:17:28 +0300

    Произведение числел от 1 до N.

---
+++ Task28_Product_of_numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает число N, и выдает 
+произведение чисел от 1 до N.*/
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {a};");
+
+int PrNum(int d)
+{
+   int zum = 1;
+   for (int i = 1; i <= d; i++) zum *= i;
+   return zum;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {a} равно: {PrNum(a)};");
