1316d35e011d0a735fe7ea381ad900b98265cf29
TeaCzir <teaczir@gmail.com>
2023-07-20 03:17:42 +0300

    Возвести число А в степень числа В. В дублях.

---
+++ Task25_Raise_to_a_degree/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.*/
+Console.Clear();
+Console.Write("Введите первое число: ");
+double A = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+double B = Convert.ToDouble(Console.ReadLine());
+
+double degree = Math.Pow(A, B);
+
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {A} и число: {B},");
+Console.WriteLine($"Число {A} в степени {B} будет равно: {degree}.");
