233e337d394a2fae92bb97f6219ea1efdc64fd78
TeaCzir <teaczir@gmail.com>
2023-07-20 04:27:33 +0300

    Колличество цифр в введенном числе.

---
+++ Task26_Digits_in_a_number/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая на вход принимает число и на выход 
+выдает колличество цифр этого числа.*/
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int Dig(int s)
+{
+   int count = 0;
+   if (s < 0) s *= (-1);
+   while (s > 0)
+   {
+      s /= 10;
+      count++;
+   }
+   return count;
+}
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {a};");
+Console.WriteLine($"Колличество цифр: {Dig(a)};");
