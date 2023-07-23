99df2eeb00e25b20c3f0491e7571acf086e17b63
TeaCzir <teaczir@gmail.com>
2023-07-23 03:10:01 +0300

    Сумма чисел на нечетных позициях.

--- Task36_Sum_of_odd/Program.cs
+++ Task36_Sum_of_odd/Program.cs
@@ -1,4 +1,44 @@
 ﻿/*Задайте массив, заполненный случайными числами. Найдите сумму элементов, стоящих 
 на нечетных позициях.*/
+Console.Clear();
+int[] Array = getRandomArray(14, 10);
+System.Console.WriteLine($"Массив из {Array.Length} элеметтов: ");
+printArray(Array);
+int SumArray = sumOfNumOddPosition(Array);
 
-Console.WriteLine("Hello, World!");
+int[] getRandomArray(int length, int deviation)
+{
+   int[] result = new int[length];
+   for (int i = 0; i < length; i++)
+   {
+      result[i] = new Random().Next(-deviation, deviation + 1);
+   }
+   return result;
+}
+void printArray(int[] arr)
+{
+   Console.ForegroundColor = ConsoleColor.Green;
+   Console.Write("[");
+   for (int i = 0; i < arr.Length; i++)
+   {
+      Console.Write(arr[i]);
+      if (i != arr.Length - 1) Console.Write(", ");
+   }
+   Console.WriteLine("]");
+   Console.WriteLine("-------------------------------------------");
+   Console.ForegroundColor = ConsoleColor.White;
+}
+int sumOfNumOddPosition(int[] arr)
+{
+int sum = 0;
+for (int i = 1; i < arr.Length; i += 2)
+{
+   sum = sum + arr[i];
+}
+
+Console.Write($"Сумма элементов с нечетным индексом равна: ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write($"{sum};");
+return sum;
+}
+Console.ForegroundColor = ConsoleColor.White;
