599714e4b164c7d303ebdec751a32c07a25e65dd
TeaCzir <teaczir@gmail.com>
2023-07-24 18:41:26 +0300

    Десятичное в двоичное.

---
+++ Task42_Decimal_to_binary1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите программу, которая будет преобразовывать десятичное 
+число в двоичное.*/
+Console.Clear();
+Console.Write("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {value};");
+
+double DecImalToBinary(int dec)
+{
+double bin = 0;
+int count = 0;
+while (dec > 0)
+{
+   bin+=dec%2* Math.Pow(10, count);
+   dec /= 2;
+   count++;
+}
+return bin;
+}
+
+Console.Write($"Оно в двоичном исчеслении будет выглядеть так: ");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine(DecImalToBinary(value));
+Console.ForegroundColor = ConsoleColor.White;
+
+
+
+
+
