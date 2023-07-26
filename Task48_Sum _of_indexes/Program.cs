da1cb4f74932f4d34aa88737016a1ae366ddb87d
TeaCzir <teaczir@gmail.com>
2023-07-26 08:26:59 +0300

    Сумма индексов.

---
+++ Task48_Sum _of_indexes/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задайте двумерный массив размера m на n, каждый элемент в массиве находится 
+по формуле: A[i][j] = i + j. Выведите полученный массив на экран.*/
+Console.Clear();
+
+int[,] Get2DArray(int rowNumber, int colNumber)
+{
+   int[,] rezult = new int[rowNumber, colNumber];
+   for (int i = 0; i < rowNumber; i++)
+   {
+      for (int j = 0; j < colNumber; j++)
+      {
+         rezult[i, j] = i + j;
+      }
+   }
+   return rezult;
+}
+
+void Prent2DArray(int[,] arrayToPrint)
+{
+   Console.Write($"[ ]\t");
+   for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+   {
+      Console.Write($"[{i}]\t");
+   }
+   Console.WriteLine();
+   for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+   {
+      Console.Write($"[{i}]\t");
+      for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+      {
+         Console.Write(arrayToPrint[i, j] + "\t");
+      }
+      Console.WriteLine();
+   }
+}
+int[,] Array = Get2DArray(5, 8);
+Prent2DArray(Array);
