8ca7f1ba80178e476c6604ddc6ddf33edadd6e94
TeaCzir <teaczir@gmail.com>
2023-07-26 07:47:37 +0300

    Вещественные числа в двухмерном массиве.

---
+++ Task47_Real_array1/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Показать двумерный массив размером m×n заполненный вещественными числами*/
+
+Console.Clear();
+int m = 5;
+int n = 8;
+int x = -100;
+int y = 100;
+
+double[,] array = new double[m, n];
+
+void FillArray(double[,] arrayToPrint)
+{
+   for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+   {
+      for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+      {
+         arrayToPrint[i, j] = new Random().NextDouble() * ((y + 1) - x) + x;
+      }
+   }
+}
+
+void Print(double[,] arrayToPrint)
+{
+   Console.Write($"[ ]\t");
+   for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+   {
+      Console.Write($"[{i}]\t");
+   }
+   Console.WriteLine();
+   for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+   {
+      Console.Write($"[{i}]\t");
+      for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+      {
+         Console.Write(Math.Round(arrayToPrint[i, j], 2) + "\t");
+      }
+      Console.WriteLine();
+   }
+}
+FillArray(array);
+Print(array);
