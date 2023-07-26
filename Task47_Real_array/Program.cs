f3f829376965902e6e68730a84b58227a5b79e1d
TeaCzir <teaczir@gmail.com>
2023-07-26 07:00:41 +0300

    Двумерный массив с вещественными элементами.

---
+++ Task47_Real_array/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задайте двумерный массив размером m×n, заполненный случайными вещественными 
+числами.*/
+Console.Clear();
+double[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)
+{   
+   double[,] rezult = new double[rowNumber, colNumber];
+   for (int i = 0; i < rowNumber; i++)
+   {
+      for (int j = 0; j < colNumber; j++)
+      {
+         rezult[i, j] = new Random().NextDouble() * ((deviation + 1) + deviation) - deviation;
+      }
+   }
+   return rezult;
+}
+
+void Prent2DArray(double[,] arrayToPrint)
+{
+   Console.Write($"[ ]\t");
+   for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+   {
+      Console.Write($"[{i}]\t");
+   }
+   Console.WriteLine();
+   for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+   {
+      Console.Write($"[{i}]\t");
+      for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+      {
+         Console.Write(Math.Round(arrayToPrint[i, j], 2) + "\t");
+      }
+      Console.WriteLine();
+   }
+}
+double[,] randomArray = GetRandom2DArray(5, 8, 10);
+Prent2DArray(randomArray);
