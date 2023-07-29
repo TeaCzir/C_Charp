770a70f9570989459742a488ea0de3c4e7c16e99
TeaCzir <teaczir@gmail.com>
2023-07-29 13:00:28 +0300

    Строки меняем со столбцами.

---
+++ Task55_Change_rows_to_columns1/Program.cs
@@ -0,0 +1,70 @@
+﻿/*Задайте двумерный массив. Напишите программу, которая заменяет строки на 
+столбцы. В случае, если это невозможно, программа должна вывести сообщение 
+для пользователя.*/
+Console.Clear();
+Console.WriteLine("Матрица изначальная:");
+Console.WriteLine();
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviatiot)
+{
+   int[,] rezult = new int[rowNumber, colNumber];
+   for (int i = 0; i < rowNumber; i++)
+   {
+      for (int j = 0; j < colNumber; j++)
+      {
+         rezult[i, j] = new Random().Next(-deviatiot, deviatiot + 1);
+      }
+   }
+   return rezult;
+}
+
+void Prent2DArray(int[,] arrayToPrint)
+{
+   Console.Write($"[ ]\t");
+   for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+   {
+      Console.Write($"[{i}]\t");
+   }
+   Console.WriteLine();
+   for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+   {
+      Console.Write($"[{i}]\t");
+      for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+      {
+         Console.Write(arrayToPrint[i, j] + "\t");
+      }
+      Console.WriteLine();
+   }
+}
+
+void RowsToColumns(int[,] matrix)
+{
+   // int[,] matrix = new int[array.GetLength(0), array.GetLength(1)];
+   if (matrix.GetLength(0) != matrix.GetLength(1))
+   {
+      Console.WriteLine("Матрица невозможна.");
+   }
+   else
+   {
+      Console.WriteLine("Матрица повернутая строки в столбцы:");
+      Console.WriteLine();
+      for (int i = 0; i < matrix.GetLength(0); i++)
+      {
+         for (int j = 0 + i; j < matrix.GetLength(1); j++)
+         {
+            int temp = matrix[j, i];
+            matrix[j, i] = matrix[i, j];
+            matrix[i, j] = temp;
+         }
+
+      }
+      Prent2DArray(matrix);
+   }
+
+}
+
+int[,] matrix = GetRandom2DArray(4, 4, 10);
+Prent2DArray(matrix);
+Console.WriteLine();
+
+RowsToColumns(matrix);
+Console.WriteLine();
