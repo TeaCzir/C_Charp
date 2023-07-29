a57138b636e8bda68db348aef69d516d26bbd7e1
TeaCzir <teaczir@gmail.com>
2023-07-29 20:55:23 +0300

    Рекурсия. Натуральные числа.

---
+++ Task63_Natural_numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задайте значение N.Напишите программу,
+ которая выведет все натуральные числа в промежутке от 1 до N.*/
+
+
+using static System.Console;
+Clear();
+Write("Введите N: ");
+int n = Convert.ToInt32(ReadLine());
+for (int i = 1; i <= n; i++)
+{
+   Write($"{i} ");
+}
+WriteLine();
+WriteLine(PrintNumbers(n));
+
+string PrintNumbers(int n)
+{
+   if (n == 1)
+   {
+      WriteLine(1);
+      return "1";
+   }
+   string s = PrintNumbers(n - 1) + ", " + n.ToString();
+   WriteLine(s);
+   return s;
+}
