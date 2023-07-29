734de05c064eddd1b92f9a2784f5a92ccc3d1dd4
TeaCzir <teaczir@gmail.com>
2023-07-29 21:13:15 +0300

    Натуральные числа до N. Рекурсивный метод.

---
+++ Task63_Natural_num/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задайте значение N.Напишите программу,
+ которая выведет все натуральные числа в промежутке от 1 до N.*/
+
+void NatureLeNum(int Number, int count = 1)
+{
+   if (Number < count)
+   {
+      return;
+   }
+   else
+   {
+      Console.Write(count + ", ");
+      NatureLeNum(Number, count + 1);
+   }
+}
+NatureLeNum(14);
