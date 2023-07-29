e46fed15dcd5f55bc6b104cf305227053f995ea9
TeaCzir <teaczir@gmail.com>
2023-07-29 21:41:00 +0300

    Натуральные числа в промежутке от M до N.

---
+++ Task65_Natur_num_in_interval/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задайте значения M и N. Напишите программу, которая выдает все натуральные числа 
+от M до N.*/
+
+Console.Clear();
+
+void RecursionNumberMN(int start, int finish)
+{
+   if (start <= finish)
+   {
+      Console.Write(start + ", ");
+      RecursionNumberMN(start + 1, finish);
+  }
+}
+RecursionNumberMN(1, 8);
+
