ac052a1c59e836b5f2a8886c4710573a76dc399f
TeaCzir <teaczir@gmail.com>
2023-07-30 14:23:55 +0300

    Число: A в степени числа: B.

---
+++ Task69_Degree_of_one_in_other/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая на вход принимает два числа A и B, 
+и возводит число А в натуральную степень B с помощью рекурсии.*/
+using System;
+using static System.Console;
+
+
+Clear();
+Write("Введите число: ");
+int A = Convert.ToInt32(ReadLine());
+Write("Введите степень: ");
+int B = Convert.ToInt32(ReadLine());
+
+WriteLine($"{Pow(A, B)}");
+
+
+int Pow(int number, int rank)
+{
+    if (rank == 0) 
+        return 1;
+    return (number * Pow(number, rank - 1));
+}
