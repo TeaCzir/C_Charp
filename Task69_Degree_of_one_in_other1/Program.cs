a633e216381fe7fed10c10a0185f1e25059e9c72
TeaCzir <teaczir@gmail.com>
2023-07-30 14:39:19 +0300

    Степень одного числа в другое.

---
+++ Task69_Degree_of_one_in_other1/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая на вход принимает два числа A и B, 
+и возводит число А в натуральную степень B с помощью рекурсии.*/
+
+
+int AToRowB(int a, int b)
+{
+   if (b == 1)
+   {
+      return a;
+   }
+   else
+   {
+      return a * AToRowB(a, b - 1);
+   }
+
+}
+
+
+
+Console.WriteLine(AToRowB(8, 4));
