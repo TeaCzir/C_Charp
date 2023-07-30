c94878599a6b7bcc37b140ebfb34df38857b9643
TeaCzir <teaczir@gmail.com>
2023-07-30 11:15:20 +0300

    Натуральные от N до 1.

---
+++ Task64_Natural_numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+натуральные числа в промежутке от N до 1.*/
+
+Console.Clear();
+int numbers = NumInp("Введите N: ");
+int count = 2;
+PrintNum(numbers, count);
+Console.Write(1);
+
+void PrintNum(int numbers, int count)
+{
+  if (count > numbers) return;
+  PrintNum(numbers, count + 1);
+  Console.Write(count + ", ");
+}
+
+int NumInp(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
