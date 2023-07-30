676af3cefe7ece4b75ba2ed37446be2f88243d9e
TeaCzir <teaczir@gmail.com>
2023-07-30 13:27:51 +0300

    Сумма цифр числа.

---
+++ Task67_Sum_of_digits_of_num/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая будет принимать на вход 
+число и возвращать сумму его цифр.*/
+
+using System;
+using static System.Console;
+
+
+Clear();
+Write("Введите число: ");
+int m = Convert.ToInt32(ReadLine());
+
+WriteLine($"Сумма цифр числа :{m} равняется: {SumNumbers(m)};");
+
+int SumNumbers(int number)
+{
+    if (number == 0)
+        return 0;
+    return number % 10 + SumNumbers(number / 10);
+}
