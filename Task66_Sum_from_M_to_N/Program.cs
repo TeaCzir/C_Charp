3a4926e8f18304b2551b92145a9e9a2723b03399
TeaCzir <teaczir@gmail.com>
2023-07-30 11:26:59 +0300

    Сумма от M до N.

---
+++ Task66_Sum_from_M_to_N/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N.*/
+
+Console.Clear();
+int m = NumInp("Введите M: ");
+int n = NumInp("Введите N: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSum(m, n, temp=0);
+
+void PrintSum(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов: {summ};");
+    return;
+  }
+  PrintSum(m, n - 1, summ);
+}
+
+int NumInp(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
